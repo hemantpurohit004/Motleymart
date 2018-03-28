73f9d4fad905cdb7be3a4d57cd98e5a191a3e86e
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2018-03-28 13:13:09 +0530

    Issue #122 fix:Push latest code

--- src/plugins/api/ecomm/ecomm/ecommupdateuserdetails.php
+++ src/plugins/api/ecomm/ecomm/ecommupdateuserdetails.php
@@ -6,7 +6,7 @@
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('_JEXEC') or die( 'Restricted access' );
+defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.plugin.plugin');
 jimport('joomla.user.helper');
@@ -20,50 +20,50 @@ jimport('joomla.user.helper');
  */
 class EcommApiResourceEcommUpdateUserDetails extends ApiResource
 {
-	/**
-	 * API Plugin for get method
-	 *
-	 * @return  avoid.
-	 */
-	public function get()
-	{
-		$this->plugin->setResponse("Please Use Post method");
-	}
+    /**
+     * API Plugin for get method
+     *
+     * @return  avoid.
+     */
+    public function get()
+    {
+        $this->plugin->setResponse("Please Use Post method");
+    }
 
-	/**
-	 * API Plugin for post method
-	 *
-	 * @return  avoid.
-	 */
-	public function post()
-	{
-		// Require helper file
-		JLoader::register('EcommService', JPATH_SITE. '/administrator/components/com_ecomm/services/ecomm.php');
+    /**
+     * API Plugin for post method
+     *
+     * @return  avoid.
+     */
+    public function post()
+    {
+        // Require helper file
+        JLoader::register('EcommUserService', JPATH_ADMINISTRATOR . '/components/com_ecomm/services/user.php');
 
-		$service  = new EcommService();
+        $service = new EcommUserService();
 
-		// Get the request body and convert it into array
-		$inputData = json_decode(file_get_contents('php://input'), true);
-		$userData = array();
+        // Get the request body and convert it into array
+        $inputData = json_decode(file_get_contents('php://input'), true);
+        $userData  = array();
 
-		$userId = $inputData['userId'];
-		$username = $inputData['username'];
-		$name = $inputData['name'];
-		$email = $inputData['email'];
-		$password = $inputData['password'];
-		$lattitude = $inputData['lattitude'];
-		$longitude = $inputData['longitude'];
-		$dob = $inputData['dob'];
+        $userId    = $inputData['userId'];
+        $username  = $inputData['username'];
+        $name      = $inputData['name'];
+        $email     = $inputData['email'];
+        $password  = $inputData['password'];
+        $lattitude = $inputData['lattitude'];
+        $longitude = $inputData['longitude'];
+        $dob       = $inputData['dob'];
 
-		foreach($inputData as $key => $value){
-			if(!empty($inputData[$key])){
-				$userData[$key] = $value;
-			}
-		}
-		//print_r($userData);die;
-		$data     = $service->ecommUpdateUserDetails($userData);
+        foreach ($inputData as $key => $value) {
+            if (!empty($inputData[$key])) {
+                $userData[$key] = $value;
+            }
+        }
+        //print_r($userData);die;
+        $data = $service->ecommUpdateUserDetails($userData);
 
-		$this->plugin->setResponse($data);
-		return true;
-	}
+        $this->plugin->setResponse($data);
+        return true;
+    }
 }
